eb7c4069ceb10d9e818f03430d6677374b168f57
Manekas Singh Sahni <manekassinghsahni@Manekas-MacBook-Air.local>
2024-12-25 03:37:01 +0000

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GreetingElement from './myGreetingApp';
+import GreetingElementwithProp from './mygreetingprop'; 
+import AppColor from './AppbackgroundColor'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppColor heading="This is first element" lbl="Name :"color="green"/> 
+    <AppColor heading="This is second element" lbl="Name :"color="blue"/> 
+    <AppColor heading="This is third third element" lbl="Name :"color="Yellow"/> 
   </React.StrictMode>
 );
 
